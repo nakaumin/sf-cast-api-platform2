5ede70ecf489b88bf82392bbbef091ea4356a26b
nakaumin <nakaumin@gmail.com>
2020-12-15 19:14:11 +0900

    [c31] completed

--- src/Serializer/Normalizer/UserNormalizer.php
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -4,17 +4,18 @@ namespace App\Serializer\Normalizer;
 
 use App\Entity\User;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
-class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+class UserNormalizer implements ContextAwareNormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
 {
-    private $normalizer;
+    use NormalizerAwareTrait;
+    // NormalizerAwareInterfaceと合わせて、$this->normalizerでデフォルトのノーマライザチェーンを呼び出せるようにするおまじない
 
-    public function __construct(ObjectNormalizer $normalizer)
-    {
-        $this->normalizer = $normalizer;
-    }
+    const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
 
     /**
      * @param User  $object  Object to normalize
@@ -26,22 +27,26 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
             $context['groups'][] = 'owner:read';  //this is 'normalized' process
         }
 
-        $data = $this->normalizer->normalize($object, $format, $context);
-        // Here: add, edit, or delete some data
-        //
+        // prevent recursion
+        $context[self::ALREADY_CALLED] = true;
 
+        $data = $this->normalizer->normalize($object, $format, $context);
 
         return $data;
     }
 
-    public function supportsNormalization($data, $format = null): bool
+    public function supportsNormalization($data, $format = null, $context = []): bool
     {
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
         return $data instanceof \App\Entity\User;
     }
 
     public function hasCacheableSupportsMethod(): bool
     {
-        return true;
+        return false;
     }
 
     private function userIsOwner(User $user)
